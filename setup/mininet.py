a75112c54d65fa1637e9c511217840f284e9cd41
Vishal Devadiga <vishaldevadiga003@gmail.com>
2023-12-03 15:45:39 +0530

    Setup mininet

---
+++ setup/mininet.py
@@ -0,0 +1,31 @@
+# Importing required libraries
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
+import json
+
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        # Topo.json is a file that contains the topology of the network
+        with open('../config/topo.json') as f:
+            topology = json.load(f)
+        # Adding hosts
+        for host in topology['hosts']:
+            self.addHost(name=host['name'], ip=host['ip'])
+        # Adding switches
+        for switch in topology['switches']:
+            self.addSwitch(name=switch['name'])
+        # Adding links
+        for link in topology['links']:
+            self.addLink(link['src'], link['dst'], delay=link['delay'], bw=link['bw'])
+
+topos = {'mytopo': (lambda: MyTopo())}
+
+if __name__ == '__main__':
+    topo = MyTopo()
+    net = Mininet(topo)
+    net.start()
+    CLI(net)
+    net.stop()
